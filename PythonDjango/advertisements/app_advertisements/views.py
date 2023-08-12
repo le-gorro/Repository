afa693cf5e272035ac323545bf9bc0a861f49363
Your Name <you@example.com>
2023-08-12 15:35:12 +0700

    комит

---
+++ PythonDjango/advertisements/app_advertisements/views.py
@@ -0,0 +1,21 @@
+﻿from django.shortcuts import render
+#from django.http import HttpResponse #return HttpResponse('успех')
+
+def index(reguest):
+    return render(reguest, 'index.html')
+
+def top_sellers(reguest):
+    return render(reguest, 'top-sellers.html')
+
+def advertisement_post(reguest):
+    return render(reguest, 'advertisement-post.html')
+
+def register(reguest):
+    return render(reguest, 'register.html')
+
+def login(reguest):
+    return render(reguest, 'login.html')
+
+def profile(reguest):
+    return render(reguest, 'profile.html')
+# Create your views here.
---
+++ PythonDjango/advertisements/app_lesson_4/views.py
@@ -0,0 +1,7 @@
+﻿from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(reguest):
+    return HttpResponse('Домашка по 4 занятию')
+
+# Create your views here.
